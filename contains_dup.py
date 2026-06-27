7b6cb4f10fe2e3ea562f6c434b15ff4c7a133219
V Ragavi Raksha <ragaviraksha165@gmail.com>
2026-06-27 18:08:58 +0530

    Delete contains_dup.py

--- contains_dup.py
+++
@@ -1,9 +0,0 @@
-arr=[2,6,3,1,4]
-def contains_dup(arr):
-    seen=set()
-    for i in arr:
-        if i in seen:
-            return True
-        seen.add(i)
-    return False
-print(contains_dup(arr))
